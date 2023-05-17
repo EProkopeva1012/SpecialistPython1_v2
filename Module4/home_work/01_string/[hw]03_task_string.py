b4c4131895ae9fb33ce62a8d31f5ae3cda01328b
EProkopeva1012 <133659236+EProkopeva1012@users.noreply.github.com>
2023-05-18 00:32:30 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,10 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+str = input("Введите строку:")
+count = 0
+sub_str = set("ауоыиэяюеё")
+for index in str:
+    if index not in sub_str:
+        count += 1
+print("Количество coгласных равно:")
+print(count)
